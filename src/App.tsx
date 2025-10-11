89c90844e8cc1a39037f4ba5a15a6ac5e2b92ea3
Sourish Ghosh <sghosh.ile.7@gmail.com>
2025-10-11 15:48:17 +0530

    Setup Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+// Basic App component
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="min-h-screen bg-white dark:bg-gray-900 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-8">
+          ClickClack
+        </h1>
+        <p className="text-gray-600 dark:text-gray-400">
+          Basic typing speed test mockup
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
